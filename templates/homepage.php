cd15f2476604af47910274e66528324f7f9397ca
Daniel Gładysz <dgladysz1@gmail.com>
2016-11-18 13:44:58 +0100

    Homepage changes

--- templates/homepage.php
+++ templates/homepage.php
@@ -8,7 +8,15 @@
 
 <main id="main" class="homepage" tabindex="-1">
 
-	<h1>Hello Word</h1>
+	<section class="hello-section element-paddings">
+		<div class="container">
+			<div class="row ">
+				<div class="col-xs-12">
+					<h1 class="hello-section__title">Harviestoun</h1><!-- /.hello-section__title -->
+				</div><!-- /.col-xs-12 -->
+			</div><!-- /.row  -->
+		</div><!-- /.container -->
+	</section><!-- /.hello-section element-paddings -->
 
 </main><!-- /#main.homepage -->
 
