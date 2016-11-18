cd3cddf2b04c5bb1512d6b263d53f2fac3b93d4a
Daniel Gładysz <dgladysz1@gmail.com>
2016-11-18 14:05:33 +0100

    Three articles

--- index.php
+++ index.php
@@ -11,6 +11,18 @@
 							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex nostrum cupiditate sint ducimus! Voluptas explicabo rem, eos, placeat perspiciatis, voluptates error veniam ea esse fugit deserunt! Nemo, nostrum! Autem, dolor.</p>
 						</div><!-- /.single-blog__excerpt content -->
 					</article><!-- /.single-blog element-magins--top -->
+					<article class="single-blog element-magins--top">
+						<h2 class="single-blog__title">Title</h2><!-- /.single-blog__title -->
+						<div class="single-blog__excerpt content">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex nostrum cupiditate sint ducimus! Voluptas explicabo rem, eos, placeat perspiciatis, voluptates error veniam ea esse fugit deserunt! Nemo, nostrum! Autem, dolor.</p>
+						</div><!-- /.single-blog__excerpt content -->
+					</article><!-- /.single-blog element-magins--top -->
+					<article class="single-blog element-magins--top">
+						<h2 class="single-blog__title">Title</h2><!-- /.single-blog__title -->
+						<div class="single-blog__excerpt content">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex nostrum cupiditate sint ducimus! Voluptas explicabo rem, eos, placeat perspiciatis, voluptates error veniam ea esse fugit deserunt! Nemo, nostrum! Autem, dolor.</p>
+						</div><!-- /.single-blog__excerpt content -->
+					</article><!-- /.single-blog element-magins--top -->
 				</div><!-- /.col-xs-12 -->
 			</div><!-- /.row -->
 		</div><!-- /.conainter -->
