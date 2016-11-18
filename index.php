a9c45264ce863e88d5a178ce703df1476a6350fd
Daniel Gładysz <dgladysz1@gmail.com>
2016-11-18 14:04:38 +0100

    Created index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <?php require_once('templates/homepage.php'); ?>
+
+<main id="index">
+	<section class="blog">
+		<div class="conainter">
+			<div class="row">
+				<div class="col-xs-12">
+					<article class="single-blog element-magins--top">
+						<h2 class="single-blog__title">Title</h2><!-- /.single-blog__title -->
+						<div class="single-blog__excerpt content">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex nostrum cupiditate sint ducimus! Voluptas explicabo rem, eos, placeat perspiciatis, voluptates error veniam ea esse fugit deserunt! Nemo, nostrum! Autem, dolor.</p>
+						</div><!-- /.single-blog__excerpt content -->
+					</article><!-- /.single-blog element-magins--top -->
+				</div><!-- /.col-xs-12 -->
+			</div><!-- /.row -->
+		</div><!-- /.conainter -->
+	</section><!-- /.blog -->
+</main><!-- /#index -->
